8f14b2583ae8e200bc49d9ccb50c8c86bd55ef7f
LM Train <layconmm@msn.com>
2020-02-28 02:19:23 -0600

    Run npm build to deploy

---
+++ client/build/precache-manifest.7c12969c564bdfa5c54d2c7e415bdb10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb75bec7ced2c79d362d",
+    "url": "/static/css/main.392572f2.chunk.css"
+  },
+  {
+    "revision": "cb75bec7ced2c79d362d",
+    "url": "/static/js/main.7a1d329e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37ed0218173e7d6f3810",
+    "url": "/static/js/2.2d4bf1da.chunk.js"
+  },
+  {
+    "revision": "ad6328c5034a97c48e20f8a53ae773e0",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed371e1330738d868a4f30d69951df12.js"
+  "/precache-manifest.7c12969c564bdfa5c54d2c7e415bdb10.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
